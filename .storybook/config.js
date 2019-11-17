007431e0ce0243493d824c66cb0227a208cb5ecd
Sam <samjgorm@gmail.com>
2019-11-17 11:06:10 +0000

    Added more config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,25 @@
-import { configure, addParameters } from '@storybook/react';
+import { configure, addParameters, addDecorator } from '@storybook/react';
+// import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs } from '@storybook/addon-knobs';
+// import { withPropsTable } from 'storybook/addon-react-docgen';
 import ecosystemTheme from './ecosystemTheme';
 
-configure(require.context('../src', true, /\.stories\.jsx$/), module);
+import './../src/global/styles/_imports.scss';
+
+// addDecorator(withA11y);
+addDecorator(withKnobs);
+// addDecorator(withPropsTable);
 
 addParameters({
   options: {
     theme: ecosystemTheme,
   },
 });
+
+const req = require.context('../src', true, /\.stories\.jsx$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+
+configure(loadStories, module);
