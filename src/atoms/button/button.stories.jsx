18778eee331d40543db951cb1f42157b592388e4
Sam <samjgorm@gmail.com>
2019-11-17 11:28:18 +0000

    Added ghost styles for button component

--- src/atoms/button/button.stories.jsx
+++ src/atoms/button/button.stories.jsx
@@ -7,35 +7,75 @@ storiesOf('Atoms/Button', module)
   .add('Primary', () => (
     <Button 
       label="Button label"
-      type="primary"
+      colour="primary"
       action={ () => console.log('Clicked the primary button') } 
     />
   ))
   .add('Secondary', () => (
     <Button
       label="Button label"
-      type="secondary"
+      colour="secondary"
       action={() => console.log('Clicked the secondary button')}
     />
   ))
   .add('Danger', () => (
     <Button
       label="Button label"
-      type="danger"
+      colour="danger"
       action={() => console.log('Clicked the danger button')}
     />
   ))
   .add('Dark', () => (
     <Button
       label="Button label"
-      type="dark"
+      colour="dark"
       action={() => console.log('Clicked the dark button')}
     />
   ))
   .add('Light', () => (
     <Button
       label="Button label"
-      type="light"
+      colour="light"
       action={() => console.log('Clicked the light button')}
+    />
+  ))
+  .add('Ghost - Primary', () => (
+    <Button
+      label="Button label"
+      colour="primary"
+      type="ghost"
+      action={() => console.log('Clicked the primary button')}
+    />
+  ))
+  .add('Ghost - Secondary', () => (
+    <Button
+      label="Button label"
+      colour="secondary"
+      type="ghost"
+      action={() => console.log('Clicked the primary button')}
+    />
+  ))
+  .add('Ghost - Danger', () => (
+    <Button
+      label="Button label"
+      colour="danger"
+      type="ghost"
+      action={() => console.log('Clicked the primary button')}
+    />
+  ))
+  .add('Ghost - Dark', () => (
+    <Button
+      label="Button label"
+      colour="dark"
+      type="ghost"
+      action={() => console.log('Clicked the primary button')}
+    />
+  ))
+  .add('Ghost - Light', () => (
+    <Button
+      label="Button label"
+      colour="light"
+      type="ghost"
+      action={() => console.log('Clicked the primary button')}
     />
   ));
--- src/atoms/button/index.jsx
+++ src/atoms/button/index.jsx
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 
 import './styles.scss';
 
-const Button = ({ label, type, action }) => (
-  <button className={ `button button--${type}` } onClick={ action }>
+const Button = ({ label, colour, type, action }) => (
+  <button className={`button ${type !== undefined ? 'button--' + type + '--' + colour : 'button--' + colour }` } onClick={ action }>
     { label }
   </button>
 );
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
+  colour: PropTypes.string.isRequired,
+  type: PropTypes.string,
   action: PropTypes.func
 }
 
