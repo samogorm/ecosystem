99ccdcf1e2ada0f40c763ba0abf16f603632db89
Sam <samjgorm@gmail.com>
2019-11-17 12:20:40 +0000

    Better structure for the button stories

--- src/atoms/button/button.stories.jsx
+++ src/atoms/button/button.stories.jsx
@@ -1,163 +1,209 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Button from './index';
+
+import './../../global/styles/stories/button.scss';
 
 const clicked = () => console.log('Clicked button...');
 
 storiesOf('Atoms/Button', module)
   .add('Default', () => (
-    <Button 
-      label="Button label"
-      colour="primary"
-      action={ () => clicked() } 
-    />
+    <Fragment>
+      <div className="button-stories-layout">
+        <div className="button-story-group">
+          <label>Primary</label>
+          <Button
+            label="Button label"
+            colour="primary"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Secondary</label>
+          <Button
+            label="Button label"
+            colour="secondary"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Danger</label>
+          <Button
+            label="Button label"
+            colour="danger"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Dark</label>
+          <Button
+            label="Button label"
+            colour="dark"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Light</label>
+          <Button
+            label="Button label"
+            colour="light"
+            action={() => clicked()}
+          />
+        </div>
+      </div>
+    </Fragment>
   ))
-  .add('Secondary', () => (
-    <Button
-      label="Button label"
-      colour="secondary"
-      action={ () => clicked() }
-    />
+  .add('Ghost', () => (
+    <Fragment>
+      <div className="button-stories-layout">
+        <div className="button-story-group">
+          <label>Primary</label>
+          <Button
+            label="Button label"
+            colour="primary"
+            type="ghost"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Secondary</label>
+          <Button
+            label="Button label"
+            colour="secondary"
+            type="ghost"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Danger</label>
+          <Button
+            label="Button label"
+            colour="danger"
+            type="ghost"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Dark</label>
+          <Button
+            label="Button label"
+            colour="dark"
+            type="ghost"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Light</label>
+          <Button
+            label="Button label"
+            colour="light"
+            type="ghost"
+            action={() => clicked()}
+          />
+        </div>
+      </div>
+    </Fragment>
   ))
-  .add('Danger', () => (
-    <Button
-      label="Button label"
-      colour="danger"
-      action={ () => clicked() }
-    />
+  .add('Small', () => (
+    <Fragment>
+      <div className="button-stories-layout">
+        <div className="button-story-group">
+          <label>Primary</label>
+          <Button
+            label="Button label"
+            colour="primary"
+            type="small"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Secondary</label>
+          <Button
+            label="Button label"
+            colour="secondary"
+            type="small"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Danger</label>
+          <Button
+            label="Button label"
+            colour="danger"
+            type="small"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Dark</label>
+          <Button
+            label="Button label"
+            colour="dark"
+            type="small"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Light</label>
+          <Button
+            label="Button label"
+            colour="light"
+            type="small"
+            action={() => clicked()}
+          />
+        </div>
+      </div>
+    </Fragment>
   ))
-  .add('Dark', () => (
-    <Button
-      label="Button label"
-      colour="dark"
-      action={() => clicked()} 
-    />
-  ))
-  .add('Light', () => (
-    <Button
-      label="Button label"
-      colour="light"
-      action={ () => clicked() }
-    />
-  ))
-  .add('Ghost - Primary', () => (
-    <Button
-      label="Button label"
-      colour="primary"
-      type="ghost"
-      action={ () => clicked() }
-    />
-  ))
-  .add('Ghost - Secondary', () => (
-    <Button
-      label="Button label"
-      colour="secondary"
-      type="ghost"
-      action={ () => clicked() }
-    />
-  ))
-  .add('Ghost - Danger', () => (
-    <Button
-      label="Button label"
-      colour="danger"
-      type="ghost"
-      action={ () => clicked() }
-    />
-  ))
-  .add('Ghost - Dark', () => (
-    <Button
-      label="Button label"
-      colour="dark"
-      type="ghost"
-      action={ () => clicked() }
-    />
-  ))
-  .add('Ghost - Light', () => (
-    <Button
-      label="Button label"
-      colour="light"
-      type="ghost"
-      action={ () => clicked() }
-    />
-  ))
-  .add('Small - Primary', () => (
-    <Button
-      label="Button label"
-      colour="primary"
-      type="small"
-      action={ () => clicked() }
-    />
-  ))
-  .add('Small - Secondary', () => (
-    <Button
-      label="Button label"
-      colour="secondary"
-      type="small"
-      action={() => clicked()}
-    />
-  ))
-  .add('Small - Danger', () => (
-    <Button
-      label="Button label"
-      colour="danger"
-      type="small"
-      action={() => clicked()}
-    />
-  ))
-  .add('Small - Dark', () => (
-    <Button
-      label="Button label"
-      colour="dark"
-      type="small"
-      action={() => clicked()}
-    />
-  ))
-  .add('Small - Light', () => (
-    <Button
-      label="Button label"
-      colour="light"
-      type="small"
-      action={() => clicked()}
-    />
-  ))
-  .add('Block - Primary', () => (
-    <Button
-      label="Button label"
-      colour="primary"
-      type="block"
-      action={() => clicked()}
-    />
-  ))
-  .add('Block - Secondary', () => (
-    <Button
-      label="Button label"
-      colour="secondary"
-      type="block"
-      action={() => clicked()}
-    />
-  ))
-  .add('Block - Danger', () => (
-    <Button
-      label="Button label"
-      colour="secondary"
-      type="block"
-      action={() => clicked()}
-    />
-  ))
-  .add('Block - Dark', () => (
-    <Button
-      label="Button label"
-      colour="dark"
-      type="block"
-      action={() => clicked()}
-    />
-  ))
-  .add('Block - Light', () => (
-    <Button
-      label="Button label"
-      colour="light"
-      type="block"
-      action={() => clicked()}
-    />
+  .add('Block', () => (
+    <Fragment>
+      <div className="button-stories-layout button-stories-layout--column">
+        <div className="button-story-group">
+          <label>Primary</label>
+          <Button
+            label="Button label"
+            colour="primary"
+            type="block"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Secondary</label>
+          <Button
+            label="Button label"
+            colour="secondary"
+            type="block"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Danger</label>
+          <Button
+            label="Button label"
+            colour="danger"
+            type="block"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Dark</label>
+          <Button
+            label="Button label"
+            colour="dark"
+            type="block"
+            action={() => clicked()}
+          />
+        </div>
+        <div className="button-story-group">
+          <label>Light</label>
+          <Button
+            label="Button label"
+            colour="light"
+            type="block"
+            action={() => clicked()}
+          />
+        </div>
+      </div>
+    </Fragment>
   ));
