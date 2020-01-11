d264a9a964631bf3e0d38e76005d4395771da511
Sam <samjgorm@gmail.com>
2020-01-11 11:00:16 +0000

    First draft of validation input helper

--- src/atoms/input/index.jsx
+++ src/atoms/input/index.jsx
@@ -1,31 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+import ErrorMessage from '../error_message';
+import { ValidateInput } from './../../helpers/ValidateInput';
 
 import './styles.scss';
 
 const Input = ({ 
   initialValue,
   name,
-  placeholder
+  placeholder,
+  validationRules
 }) => {
   let [value, setValue] = useState(initialValue);
+  let [isValid, setIsValid] = useState(true);
+  let [validationItems, setValidationItems] = useState([]);
 
   return (
-    <input
-      type="text"
-      value={ value }
-      name={ name }
-      onChange={ (event) => setValue(event.target.value) }
-      placeholder={ placeholder }
-      className="input"
-    />
+    <div>
+      <input
+        type="text"
+        value={ value }
+        name={ name }
+        onChange={ (event) => setValue(event.target.value) }
+        onBlur={() => {
+          const validationItems = ValidateInput(value, validationRules);
+          const isValidInput = validationItems.every(item => item.valid === true);
+
+          setIsValid(isValidInput);
+          setValidationItems(validationItems);
+        }}
+        placeholder={ placeholder }
+        className={ isValid ? 'input' : 'input input--error' }
+      />
+
+      {!isValid && 
+        validationItems.filter(item => !item.valid).map(item => (
+          <ErrorMessage
+            message={item.message}
+          />
+        ))
+      }
+    </div>
   );
 }
+
+// Input.defaultProps = {
+//   validationRules: []
+// };
 
 Input.propTypes = {
   initialValue: PropTypes.any.isRequired,
   name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string
-}
+  placeholder: PropTypes.string,
+  validationRules: PropTypes.array
+};
 
 export default Input;
--- src/helpers/ValidateInput.js
+++ src/helpers/ValidateInput.js
@@ -1,3 +1,56 @@
-export const ValidateInput = (value, rules) => {
-  console.log('validating...' + ' ' + value);
+const isEmailValid = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+const isTypeValid = (value, type) => typeof value === type;
+const isLengthValid = (length, maxLength) => length < maxLength + 1;
+const isRequired = value => (value !== null) && (value !== 'undefined') && (value.length > 0);
+
+const validateType = (value, type) => {
+  switch(type) {
+    case 'email':
+      return {
+        rule: 'type',
+        value: value,
+        message: 'Please enter an Email Address.',
+        valid: isEmailValid(value)
+      }
+    case 'number':
+      return {
+        rule: 'type',
+        value: value,
+        message: 'Please enter a number.',
+        valid: isTypeValid(value, 'number')
+      }
+    default:
+      return {
+        rule: 'type',
+        value: value,
+        message: 'Please enter a string.',
+        valid: isTypeValid(value, 'string')
+      }
+  }
 };
+
+export const ValidateInput = (value, rules) => {
+  const validation = [];
+
+  if (rules.type !== 'any') validation.push(validateType(value, rules.type));
+
+  if (rules.required) validation.push({
+    rule: 'required',
+    value: rules.required,
+    message: 'This field is required.',
+    valid: isRequired(value)
+  });
+
+  if (rules.length !== null) validation.push({
+    rule: 'length',
+    value: rules.length,
+    message: `Max length is ${rules.length}.`,
+    valid: isLengthValid(value.length, rules.length)
+  });
+
+  return validation;
+}
