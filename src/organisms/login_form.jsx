064acdb8a093a0c91a3b7800725aa37e1cbe3293
Sam <samjgorm@gmail.com>
2020-01-11 16:17:14 +0000

    Add skeleton markup for login form component

---
+++ src/organisms/login_form.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+const LoginForm = ({}) => {
+
+};
+
+export default LoginForm;
+
+LoginForm.propTypes = {
+  
+};
